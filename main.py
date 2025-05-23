660d6e4f7dfaf114e5b6c331142ec541b82cb8d8
zisophypsych <ziyaulhaqkhan60@gmail.com>
2025-05-23 14:32:16 +0530

    Added some pygame options to open a window, color, using infinite loop.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame 
+from constants import *
 
 def main():
-    print("Starting Asteriods!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
