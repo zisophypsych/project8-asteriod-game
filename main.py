9e6eff7ef4d8806d3a932e67a75b41140178e69f
zisophypsych <ziyaulhaqkhan60@gmail.com>
2025-05-23 15:01:44 +0530

    Set frame speed, Added some code for visual objects

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,9 @@ def main():
         
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
     print("Starting Asteroids!")
