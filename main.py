0b2f0e9d98cb710c87373d6999e52556d6d2f7e2
zisophypsych <ziyaulhaqkhan60@gmail.com>
2025-05-23 15:26:33 +0530

    Added player as triangle, made class and it's methods to draw it

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame 
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -13,9 +15,11 @@ def main():
                 return
         
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
+        
 
 
 
