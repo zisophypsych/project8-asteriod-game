0c86fa32919803dd93df2976d9a9b2b9cb030f74
zisophypsych <ziyaulhaqkhan60@gmail.com>
2025-05-23 19:48:29 +0530

    Added Asteroids and their collision logic

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def check_collisions(self, circle):
+        distance = pygame.Vector2.distance_to(self.position, circle.position)
+        if distance <= (self.radius + circle.radius):
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame 
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from circleshape import *
+
 
 def main():
     pygame.init()
@@ -9,8 +14,12 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)   
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)  
+    asteroid_field = AsteroidField() 
 
     while True:
         for event in pygame.event.get():
@@ -28,6 +37,12 @@ def main():
 
         for thing in updatable:
             thing.update(dt)
+
+        for thing in asteroids:
+            if thing.check_collisions(player):
+                print("Game Over!")
+                sys.exit()
+            
 
         pygame.display.flip()
 
